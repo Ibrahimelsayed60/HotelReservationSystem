efb4242166fba3322c53c758b81f38a89f244601
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-04 16:05:17 +0300

    Add CreateRoomEndpoint

---
+++ src/Modules/Room/Room/Rooms/Features/CreateRoom/CreateRoomEndpoint.cs
@@ -0,0 +1,42 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Room.Rooms.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room.Rooms.Features.CreateRoom
+{
+
+    public record CreateRoomRequest(RoomDto Room);
+
+    public record CreateRoomResponse(Guid Id);
+
+    public class CreateRoomEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/Rooms", async (CreateRoomRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateRoomCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateRoomResponse>();
+
+                return Results.Created($"/Rooms/{response.Id}", response);
+            })
+                .WithName("CreateRoom")
+                .Produces<CreateRoomResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Room")
+                .WithDescription("Create Room");
+        }
+    }
+}
---
+++ src/Modules/Room/Room/Rooms/Features/CreateRoom/CreateRoomHandler.cs
@@ -0,0 +1,46 @@
+﻿using Room.Data;
+using Room.Rooms.Dtos;
+using Room.Rooms.Models;
+using Shared.Contracts.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Room.Rooms.Features.CreateRoom
+{
+    public record CreateRoomCommand(RoomDto room):ICommand<CreateRoomResult>;
+    public record CreateRoomResult(Guid Id);
+
+    public class CreateRoomHandler(RoomDbContext dbContext) : ICommandHandler<CreateRoomCommand, CreateRoomResult>
+    {
+        public async Task<CreateRoomResult> Handle(CreateRoomCommand request, CancellationToken cancellationToken)
+        {
+            var room = CreateNewRoom(request.room);
+
+            dbContext.Rooms.Add(room);
+
+            await dbContext.SaveChangesAsync();
+
+            return new CreateRoomResult(room.Id);
+        }
+
+        private Models.Rooms CreateNewRoom(RoomDto roomDto) 
+        {
+            var room = Models.Rooms.CreateRoom
+                (
+                Guid.NewGuid(),
+                roomDto.Name,
+                roomDto.Type,
+                roomDto.Price,
+                roomDto.Description,
+                roomDto.Capacity,
+                roomDto.IsAvailable
+                );
+
+            return room;
+        }
+    }
+}
