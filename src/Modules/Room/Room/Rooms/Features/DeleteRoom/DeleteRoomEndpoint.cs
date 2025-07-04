51d914a4d13d3e92d7a17b7225c5d3bb71d954c3
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-04 16:55:55 +0300

    Add Delete Room Endpoint

---
+++ src/Modules/Room/Room/Rooms/Features/DeleteRoom/DeleteRoomEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room.Rooms.Features.DeleteRoom
+{
+
+    public record DeleteRoomResponse(bool IsSuccess);
+
+    public class DeleteRoomEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/Rooms/{Id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteRoomCommand(Id));
+
+                var response = result.Adapt<DeleteRoomResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("DeleteRoom")
+                .Produces<DeleteRoomResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("DeleteRoom")
+                .WithDescription("Delete Room");
+        }
+    }
+}
---
+++ src/Modules/Room/Room/Rooms/Features/DeleteRoom/DeleteRoomHandler.cs
@@ -0,0 +1,44 @@
+﻿using Room.Data;
+using Room.Rooms.Models;
+using Shared.Contracts.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Room.Rooms.Features.DeleteRoom
+{
+
+    public record DeleteRoomCommand(Guid RoomId): ICommand<DeleteRoomResult>;
+
+    public record DeleteRoomResult(bool IsSuccess);
+
+    public class DeleteRoomHandler(RoomDbContext dbContext) : ICommandHandler<DeleteRoomCommand, DeleteRoomResult>
+    {
+        public async Task<DeleteRoomResult> Handle(DeleteRoomCommand request, CancellationToken cancellationToken)
+        {
+            var room = await dbContext.Rooms.FindAsync(request.RoomId, cancellationToken);
+
+            if(room == null)
+            {
+                throw new Exception($"Room not found: {request.RoomId}");
+            }
+
+            DeleteExistedRoom(room);
+
+            dbContext.Rooms.Update(room);
+
+            await dbContext.SaveChangesAsync();
+
+            return new DeleteRoomResult(true);
+
+        }
+
+        private void DeleteExistedRoom(Models.Rooms room)
+        {
+            room.DeleteRoom(room.Id);
+        }
+    }
+}
