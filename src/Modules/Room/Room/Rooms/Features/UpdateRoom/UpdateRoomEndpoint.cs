88a7a8bb57d41f822509df537c3416834db9065c
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-04 16:29:24 +0300

    added Update Room Endpoint

---
+++ src/Modules/Room/Room/Rooms/Features/UpdateRoom/UpdateRoomEndpoint.cs
@@ -0,0 +1,44 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Room.Rooms.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room.Rooms.Features.UpdateRoom
+{
+
+    public record UpdateRoomRequest(RoomDto Room);
+
+    public record UpdateRoomResponse(bool IsSuccess);
+
+    public class UpdateRoomEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/Rooms", async (UpdateRoomRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateRoomCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateRoomResponse>();
+
+                return Results.Ok(response);
+
+            })
+                .WithName("UpdateRoom")
+                .Produces<UpdateRoomResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Update Room")
+                .WithDescription("Update Room");
+        }
+    }
+}
--- src/Modules/Room/Room/Rooms/Features/UpdateRoom/UpdateRoomHandler.cs
+++ src/Modules/Room/Room/Rooms/Features/UpdateRoom/UpdateRoomHandler.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Room.Data;
+using Room.Rooms.Dtos;
+using Shared.Contracts.CQRS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Room.Rooms.Features.UpdateRoom
 {
-    internal class UpdateRoomHandler
+
+    public record UpdateRoomCommand(RoomDto Room):ICommand<UpdateRoomResult>;
+
+    public record UpdateRoomResult(bool IsSuccess);
+
+    public class UpdateRoomHandler(RoomDbContext dbContext) : ICommandHandler<UpdateRoomCommand, UpdateRoomResult>
     {
+        public async Task<UpdateRoomResult> Handle(UpdateRoomCommand request, CancellationToken cancellationToken)
+        {
+            var room = await dbContext.Rooms.FindAsync(request.Room.Id, cancellationToken);
+
+            if (room == null)
+            {
+                throw new Exception($"Room not found {request.Room.Id}");
+            }
+
+            UpdateRoomWithNewValues(room, request.Room);
+
+            dbContext.Rooms.Update(room);
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new UpdateRoomResult(true);
+        }
+
+        private void UpdateRoomWithNewValues(Models.Rooms room1, RoomDto room2)
+        {
+            room1.UpdateRoom(
+                room2.Name,
+                room2.Type,
+                room2.Price,
+                room2.Description,
+                room2.Capacity,
+                room2.IsAvailable
+                );
+        }
     }
 }
