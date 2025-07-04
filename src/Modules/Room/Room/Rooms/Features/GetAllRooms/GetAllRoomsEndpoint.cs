133cd7cc04408c92b8abffb309e371fe86243c8d
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-04 15:16:27 +0300

    add endpoint to get all rooms

---
+++ src/Modules/Room/Room/Rooms/Features/GetAllRooms/GetAllRoomsEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Room.Rooms.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room.Rooms.Features.GetAllRooms
+{
+
+    public record GetAllRoomsResponse(IEnumerable<RoomDto> rooms);
+
+    public class GetAllRoomsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/Rooms", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetAllRoomsQuery());
+
+                var response = new GetAllRoomsResponse(result.rooms);
+
+                return Results.Ok(response);
+
+            })
+            .WithName("GetRooms")
+            .Produces<GetAllRoomsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Rooms")
+            .WithDescription("Get Rooms");
+        }
+    }
+}
--- src/Modules/Room/Room/Rooms/Features/GetAllRooms/GetAllRoomsHandler.cs
+++ src/Modules/Room/Room/Rooms/Features/GetAllRooms/GetAllRoomsHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Mapster;
+using Microsoft.EntityFrameworkCore;
+using Room.Data;
+using Room.Rooms.Dtos;
+using Shared.Contracts.CQRS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,19 @@ using System.Threading.Tasks;
 
 namespace Room.Rooms.Features.GetAllRooms
 {
-    public class GetAllRoomsHandler
+    public record GetAllRoomsQuery():IQuery<GetAllProductsResult>;
+
+    public record GetAllProductsResult(IEnumerable<RoomDto> rooms);
+
+    public class GetAllRoomsHandler(RoomDbContext dbContext) : IQueryHandler<GetAllRoomsQuery, GetAllProductsResult>
     {
+        public async Task<GetAllProductsResult> Handle(GetAllRoomsQuery request, CancellationToken cancellationToken)
+        {
+            var rooms = await dbContext.Rooms.AsNoTracking().OrderBy(r => r.Name).ToListAsync(cancellationToken);
+
+            var roomsDto = rooms.Adapt<List<RoomDto>>();
+
+            return new GetAllProductsResult(roomsDto);
+        }
     }
 }
