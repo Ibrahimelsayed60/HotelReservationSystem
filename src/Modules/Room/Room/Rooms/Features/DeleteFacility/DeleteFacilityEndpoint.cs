066b3b6527d09611462c6537aae189ad7761dbf0
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-05 22:35:08 +0300

    Add Delete Facility Endpoint

---
+++ src/Modules/Room/Room/Rooms/Features/DeleteFacility/DeleteFacilityEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room.Rooms.Features.DeleteFacility
+{
+    public record DeleteFacilityResponse(bool IsSuccess);
+
+    public class DeleteFacilityEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/Facilities/{id}", async (Guid id, ISender sender) =>
+            {
+                var command = new DeleteFacilityCommand(id);
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<DeleteFacilityResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("DeleteFacility")
+                .Produces<DeleteFacilityResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Delete Facility")
+                .WithDescription("Delete Facility");
+        }
+    }
+}
---
+++ src/Modules/Room/Room/Rooms/Features/DeleteFacility/DeleteFacilityHandler.cs
@@ -0,0 +1,36 @@
+﻿using Room.Data;
+using Shared.Contracts.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Room.Rooms.Features.DeleteFacility
+{
+    public record DeleteFacilityCommand(Guid Id):ICommand<DeleteFacilityResult>;
+
+    public record DeleteFacilityResult(bool IsSuccess);
+
+    public class DeleteFacilityHandler(RoomDbContext dbContext) : ICommandHandler<DeleteFacilityCommand, DeleteFacilityResult>
+    {
+        public async Task<DeleteFacilityResult> Handle(DeleteFacilityCommand request, CancellationToken cancellationToken)
+        {
+            var facility = await dbContext.Facilities.FindAsync(request.Id, cancellationToken);
+
+            if (facility == null)
+            {
+                throw new Exception($"Facility not found {request.Id}");
+            }
+
+            facility.IsDeleted = true;
+
+            dbContext.Facilities.Update(facility);
+
+            await dbContext.SaveChangesAsync();
+
+            return new DeleteFacilityResult(true);
+        }
+    }
+}
