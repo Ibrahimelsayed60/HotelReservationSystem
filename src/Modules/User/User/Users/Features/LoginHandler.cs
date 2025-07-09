16678102dfedae692c681f92165bcb16dc1e73ed
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-09 06:03:26 +0300

    add login handler feature

--- src/Modules/User/User/Users/Features/LoginHandler.cs
+++ src/Modules/User/User/Users/Features/LoginHandler.cs
@@ -1,21 +1,60 @@
-﻿using Shared.Contracts.CQRS;
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Shared.Contracts.CQRS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using User.Users.Events;
+using User.Users.Models;
+using User.Users.Service;
 
 namespace User.Users.Features
 {
     public record LoginCommand(string Email, string Password):ICommand<LoginResult>;
 
-    public record LoginResult(string Token, string Email, string FullName, List<String> Roles);
+    public record LoginResult(string Token, string Email, string FullName, List<string> Roles);
 
     public class LoginHandler : ICommandHandler<LoginCommand, LoginResult>
     {
-        public Task<LoginResult> Handle(LoginCommand request, CancellationToken cancellationToken)
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ITokenService _tokenService;
+        private readonly IMediator _mediator;
+
+        public LoginHandler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService, IMediator mediator)
         {
-            throw new NotImplementedException();
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+            _mediator = mediator;
+        }
+
+        public async Task<LoginResult> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null || !user.IsActive)
+                throw new Exception("Invalid credientials");
+
+            var validPassword = await _userManager.CheckPasswordAsync(user, request.Password);
+            if (!validPassword)
+                throw new Exception("Invalid credientials");
+
+            var roles = (await _userManager.GetRolesAsync(user)).ToList();
+            var token = await _tokenService.GenerateTokenAsync(user, _userManager);
+
+            await _mediator.Publish(new UserLoggedInEvent(user.Id, user.Email));
+
+
+            return new LoginResult
+            (
+                token,
+                user.Email,
+                user.FullName,
+                roles
+            );
+
         }
     }
 }
