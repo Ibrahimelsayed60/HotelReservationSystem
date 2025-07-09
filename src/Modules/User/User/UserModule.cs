d1216d3148fcd1aaf5d848be8aa8436c92fcb4e5
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-09 05:51:48 +0300

    add token service

--- src/Modules/User/User/UserModule.cs
+++ src/Modules/User/User/UserModule.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Shared.Data.Interceptors;
 using Shared.Data.Seed;
 using System;
@@ -39,6 +41,26 @@ namespace User
                 .AddDefaultTokenProviders();
 
             services.AddScoped<IDataSeeder, UserContextSeed>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateAudience = true,
+                        ValidAudience = configuration["JWT:ValidAudience"],
+                        ValidateIssuer = true,
+                        ValidIssuer = configuration["JWT:ValidIssuer"],
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"])),
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.FromDays(double.Parse(configuration["JWT:DurationInDays"]))
+                    };
+                });
 
 
             return services;
---
+++ src/Modules/User/User/Users/Service/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Users.Models;
+
+namespace User.Users.Service
+{
+    public interface ITokenService
+    {
+
+        Task<string> GenerateTokenAsync(AppUser user, UserManager<AppUser> userManager);
+
+    }
+}
