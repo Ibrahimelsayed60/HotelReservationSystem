24f7755d5c3a0a2f27d1616702d5bfd8e92c60a6
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-12 15:59:16 +0300

    add Domain Event handlers for events in reservation module

---
+++ src/Modules/Reservation/Reservation/Reservations/EventHandlers/ReservationCheckedInEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Reservation.Reservations.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.Reservations.EventHandlers
+{
+    public class ReservationCheckedInEventHandler(ILogger<ReservationCheckedInEvent> logger) : INotificationHandler<ReservationCheckedInEvent>
+    {
+        public Task Handle(ReservationCheckedInEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event handled {DomainEvent}", notification.GetType().Name);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Modules/Reservation/Reservation/Reservations/EventHandlers/ReservationPaidEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Reservation.Reservations.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.Reservations.EventHandlers
+{
+    public class ReservationPaidEventHandler(ILogger<ReservationPaidEventHandler> logger) : INotificationHandler<ReservationPaidEvent>
+    {
+        public Task Handle(ReservationPaidEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event handled {DomainEvent}", notification.GetType().Name);
+            return Task.CompletedTask;
+        }
+    }
+}
