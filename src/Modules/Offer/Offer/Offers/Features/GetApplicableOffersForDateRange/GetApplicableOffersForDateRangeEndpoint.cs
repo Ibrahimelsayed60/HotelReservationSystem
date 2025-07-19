868540730317023f34e771444af696bdffc63923
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-19 09:24:41 +0300

    add endpoint in offer module

---
+++ src/Modules/Offer/Offer/Offers/Features/GetApplicableOffersForDateRange/GetApplicableOffersForDateRangeEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Offer.Offers.Dtos;
+using Offer.Offers.Features.GetAllOffers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Offer.Offers.Features.GetApplicableOffersForDateRange
+{
+
+    public record GetApplicableOffersForDateRangeResponse(IEnumerable<OfferDto> Offers);
+
+    public class GetApplicableOffersForDateRangeEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/offers/applicable", async (DateTime checkInDate, DateTime checkOutDate, ISender sender) =>
+            {
+                var result = await sender.Send(new GetApplicableOffersForDateRangeQuery(checkInDate, checkOutDate));
+
+                var response = new GetApplicableOffersForDateRangeResult(result.Offers);
+
+                return Results.Ok(response);
+            })
+                .WithName("GetApplicableOffersForDateRange")
+                .Produces<GetApplicableOffersForDateRangeResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Applicable Offers For Date Range")
+                .WithDescription("Get Applicable Offers For Date Range")
+                ;
+        }
+    }
+}
--- src/Modules/Offer/Offer/Offers/Features/GetApplicableOffersForDateRange/GetApplicableOffersForDateRangeHandler.cs
+++ src/Modules/Offer/Offer/Offers/Features/GetApplicableOffersForDateRange/GetApplicableOffersForDateRangeHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Mapster;
+using Microsoft.EntityFrameworkCore;
+using Offer.Data;
+using Offer.Offers.Dtos;
+using Shared.Contracts.CQRS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,20 @@ using System.Threading.Tasks;
 
 namespace Offer.Offers.Features.GetApplicableOffersForDateRange
 {
-    internal class GetApplicableOffersForDateRangeHandler
+    public record GetApplicableOffersForDateRangeQuery(DateTime CheckInDate, DateTime CheckOutDate) :IQuery<GetApplicableOffersForDateRangeResult>;
+
+    public record GetApplicableOffersForDateRangeResult(IEnumerable<OfferDto> Offers);
+
+    public class GetApplicableOffersForDateRangeHandler(OfferDbContext dbContext) : IQueryHandler<GetApplicableOffersForDateRangeQuery, GetApplicableOffersForDateRangeResult>
     {
+        public async Task<GetApplicableOffersForDateRangeResult> Handle(GetApplicableOffersForDateRangeQuery request, CancellationToken cancellationToken)
+        {
+            var offers = await dbContext.Offers.Where(o => request.CheckInDate >= o.StartDate && request.CheckOutDate <= o.EndDate).ToListAsync();
+
+            var offersDto = offers.Adapt<List<OfferDto>>();
+
+            return new GetApplicableOffersForDateRangeResult(offersDto);
+
+        }
     }
 }
