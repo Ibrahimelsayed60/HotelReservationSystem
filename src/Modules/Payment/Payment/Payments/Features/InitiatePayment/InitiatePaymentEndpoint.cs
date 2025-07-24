05a174bd58461ac94d0c98761cee37efd1aa56df
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-07-24 17:14:28 +0300

    add endpoint for initiate payment

---
+++ src/Modules/Payment/Payment/Payments/Features/InitiatePayment/InitiatePaymentEndpoint.cs
@@ -0,0 +1,44 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Payments.Features.InitiatePayment
+{
+    public record InitiatePaymentRequest(Guid ReservationId,
+    Guid UserId,
+    decimal Amount);
+
+    public record InitiatePaymentResponse(string frame);
+
+    internal class InitiatePaymentEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/payment/initiate", async (
+            InitiatePaymentRequest request,
+            ISender sender,
+            CancellationToken cancellationToken) =>
+            {
+                var result = await sender.Send(new InitiatePaymentCommand(request.ReservationId, request.UserId, request.Amount));
+
+                var response = new InitiatePaymentResponse(result.frame);
+
+                return Results.Ok(response);
+
+            })
+                .WithName("InitiatePayment")
+                .Produces<InitiatePaymentResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("InitiatePayment")
+                .WithDescription("InitiatePayment")
+                ;
+        }
+    }
+}
